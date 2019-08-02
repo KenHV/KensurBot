30f980dd584abe46dde83b20d557da70f34969d6
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-08-02 21:38:10 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -21,7 +21,7 @@ from telethon.tl.types import MessageEntityMentionName
 
 from cowpy import cow
 
-from userbot import CMD_HELP, ZALG_LIST
+from userbot import CMD_HELP, ZALG_LIST, BOTLOG, BOTLOG_CHATID
 from userbot.events import register
 
 # ================= CONSTANT =================
