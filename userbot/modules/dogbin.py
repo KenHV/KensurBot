0619fc4855713678a6817df22b075a955672d9d1
Kryptic <50475008+shxnpie@users.noreply.github.com>
2019-08-16 13:03:49 +0530

    dogbin.py: Removed Master

--- userbot/modules/dogbin.py
+++ userbot/modules/dogbin.py
@@ -22,7 +22,7 @@ async def paste(pstl):
         reply_id = pstl.reply_to_msg_id
 
         if not match and not reply_id:
-            await pstl.edit("Master,please give me something to paste.")
+            await pstl.edit("Elon Musk said I cannot paste void.")
             return
 
         if match:
