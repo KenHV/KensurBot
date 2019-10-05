8b2948a4318c03ed7466b90217d1e476705eb015
bien2004official <52122810+bien2004official@users.noreply.github.com>
2019-10-05 21:19:52 +0700

    Fixed typo

--- termux_install.sh
+++ termux_install.sh
@@ -15,7 +15,7 @@ sleep 5
 clear
 
 pkg update && pkg upgrade -y
-pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebplibxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
+pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
 git clone https://github.com/AvinashReddy3108/PaperplaneExtended.git
 cd PaperplaneExtended
