f19da390546cb5284272fda940d016a206ef4dc6
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-10-21 23:06:51 +0530

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -38,7 +38,7 @@ async def upstream(ups):
     "For .update command, check if the bot is up to date, update if specified"
     await ups.edit("`Checking for updates, please wait....`")
     conf = ups.pattern_match.group(1)
-    off_repo = 'https://github.com/AvinashReddy3108/PaperplaneExtended.git'
+    off_repo = 'https://github.com/mkaraniya/PaperplaneExtended.git'
 
     try:
         txt = "`Oops.. Updater cannot continue due to some problems occured`\n\n**LOGTRACE:**\n"
