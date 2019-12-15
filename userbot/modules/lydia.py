e9dede0e4b5e2075915568db678cbc8b9f1316b2
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-15 19:41:15 +0530

    Update lydia.py

--- userbot/modules/lydia.py
+++ userbot/modules/lydia.py
@@ -2,6 +2,8 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
+# credit goes to @snapdragon and @devpatel_73 for making it work on this userbot.
+#
 """
 Userbot module to help you manage a group
 """
