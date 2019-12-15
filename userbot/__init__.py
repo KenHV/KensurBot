97de5c738c07c00fc1629c0b59046f3431c882ef
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-15 21:40:34 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -72,6 +72,11 @@ HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
 HEROKU_APPNAME = os.environ.get("HEROKU_APPNAME", None)
 HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 
+# Custom (forked) repo URL for updater.
+UPSTREAM_REPO_URL = os.environ.get(
+    "UPSTREAM_REPO_URL",
+    "https://github.com/mkaraniya/OpenUserBot.git")
+
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
 
