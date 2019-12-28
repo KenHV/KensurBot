348df9c243a07d82ff0a73efd96905bc5c393564
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2019-12-28 12:46:08 +0530

    Update lydia.py

--- userbot/modules/lydia.py
+++ userbot/modules/lydia.py
@@ -10,7 +10,7 @@ Userbot module to help you manage a group
 
 import coffeehouse
 import asyncio
-from userbot import LYDIA_API_KEY
+from userbot import LYDIA_API_KEY, CMD_HELP
 from userbot.events import register
 from telethon import events
 
@@ -87,7 +87,6 @@ async def user(event):
         return
 
     
-    
 CMD_HELP.update({
     "lydia":
     ".addcf <username/reply>\
@@ -97,3 +96,4 @@ CMD_HELP.update({
 \n\n.repcf <username/reply>\
 \nUsage: starts lydia repling to perticular person in the chat."
 })
+
