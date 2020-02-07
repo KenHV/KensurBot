0d7f02adbc7a6549e840cd67f39caecf0413a21b
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-02-07 11:40:48 +0530

    Update and rename updater.py to changelog.py

--- userbot/modules/changelog.py
+++ userbot/modules/changelog.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 """
-This module updates the userbot based on Upstream revision
+This module updates the userbot based on chtream revision
 """
 
 from os import remove, execl
@@ -33,76 +33,76 @@ async def is_off_br(br):
     return
 
 
-@register(outgoing=True, pattern="^.update(?: |$)(.*)")
-async def upstream(ups):
+@register(outgoing=True, pattern="^.chl(?: |$)(.*)")
+async def chtream(ch):
     "For .update command, check if the bot is up to date, update if specified"
-    await ups.edit("`Checking for updates, please wait....`")
-    conf = ups.pattern_match.group(1).lower()
+    await ch.edit("`Checking for updates, please wait....`")
+    conf = ch.pattern_match.group(1).lower()
     off_repo = 'https://github.com/mkaraniya/OpenUserBot.git'
 
     try:
         txt = "`Oops.. Updater cannot continue due to some problems occured`\n\n**LOGTRACE:**\n"
         repo = Repo()
     except NoSuchPathError as error:
-        await ups.edit(f'{txt}\n`directory {error} is not found`')
+        await ch.edit(f'{txt}\n`directory {error} is not found`')
         return
     except GitCommandError as error:
-        await ups.edit(f'{txt}\n`Early failure! {error}`')
+        await ch.edit(f'{txt}\n`Early failure! {error}`')
         return
     except InvalidGitRepositoryError:
         repo = Repo.init()
-        await ups.edit(
+        await ch.edit(
             "`Warning: Force-Syncing to the latest stable code from repo.`\
             \nI may lose my downloaded files during this update."
         )
-        origin = repo.create_remote('upstream', off_repo)
+        origin = repo.create_remote('chtream', off_repo)
         origin.fetch()
         repo.create_head('sql-extended', origin.refs.master)
         repo.heads.master.checkout(True)
 
     ac_br = repo.active_branch.name
     if not await is_off_br(ac_br):
-        await ups.edit(
+        await ch.edit(
             f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). \
             in that case, Updater is unable to identify which branch is to be merged. \
             please checkout to any official branch`')
         return
 
     try:
-        repo.create_remote('upstream', off_repo)
+        repo.create_remote('chtream', off_repo)
     except BaseException:
         pass
 
-    ups_rem = repo.remote('upstream')
-    ups_rem.fetch(ac_br)
-    changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+    ch_rem = repo.remote('chtream')
+    ch_rem.fetch(ac_br)
+    changelog = await gen_chlog(repo, f'HEAD..chtream/{ac_br}')
 
     if not changelog:
-        await ups.edit(f'\n`Your BOT is` **up-to-date** `with` **{ac_br}**\n')
+        await ch.edit(f'\n`Your BOT is` **up-to-date** `with` **{ac_br}**\n')
         return
 
-    if conf != "now":
+    if conf != "w":
         changelog_str = f'**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`'
         if len(changelog_str) > 4096:
-            await ups.edit("`Changelog is too big, sending it as a file.`")
+            await ch.edit("`Changelog is too big, sending it as a file.`")
             file = open("output.txt", "w+")
             file.write(changelog_str)
             file.close()
-            await ups.client.send_file(
-                ups.chat_id,
+            await ch.client.send_file(
+                ch.chat_id,
                 "output.txt",
-                reply_to=ups.id,
+                reply_to=ch.id,
             )
             remove("output.txt")
         else:
-            await ups.edit(changelog_str)
-        await ups.respond(
-            "`do \".update now\" to update\nDon't if using Heroku`")
+            await ch.edit(changelog_str)
+        await ch.respond(
+            "`do \".update \" to update\nif using Heroku`")
         return
 
-    await ups.edit('`New update found, updating...`')
-    ups_rem.fetch(ac_br)
-    await ups.edit('`Successfully Updated!\n'
+    await ch.edit('`New update found, updating...`')
+    ch_rem.fetch(ac_br)
+    await ch.edit('`Successfully Updated!\n'
                    'Bot is restarting... Wait for a second!`')
     await install_requirements()
     await bot.disconnect()
@@ -116,6 +116,6 @@ CMD_HELP.update({
     'update':
     ".update\
 \nUsage: Checks if the main userbot repository has any updates and shows a changelog if so.\
-\n\n.update now\
+\n\n.update w\
 \nUsage: Updates your userbot, if there are any updates in the main userbot repository."
 })
