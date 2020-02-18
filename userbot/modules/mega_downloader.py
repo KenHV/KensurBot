bc0a60189ede8f0ba2bda8f4ba5318d78d410055
Adek Maulana <adek@techdro.id>
2020-02-18 19:52:54 +0700

    mega_downloader: handle special characters in file_name
    also handle better file
    
    Change-Id: I190f3f28420e52c6424d7bb8b61fd93e6feebd21

--- userbot/modules/mega_downloader.py
+++ userbot/modules/mega_downloader.py
@@ -26,20 +26,24 @@ import json
 import wget
 import os
 
+from os.path import exists
+
 from userbot import CMD_HELP
 from userbot.events import register
 
 
 def subprocess_run(cmd):
+    reply = ''
     subproc = Popen(cmd, stdout=PIPE, stderr=PIPE,
                     shell=True, universal_newlines=True)
     talk = subproc.communicate()
     exitCode = subproc.returncode
     if exitCode != 0:
-        print('An error was detected while running the subprocess:\n'
-              'exit code: %d\n'
-              'stdout: %s\n'
-              'stderr: %s' % (exitCode, talk[0], talk[1]))
+        reply += ('An error was detected while running the subprocess:\n'
+                  f'exit code: {exitCode}\n'
+                  f'stdout: {talk[0]}\n'
+                  f'stderr: {talk[1]}')
+        return reply
     return talk
 
 
@@ -75,7 +79,6 @@ def mega_download(url: str) -> str:
     result = subprocess_run(cmd)
     try:
         data = json.loads(result[0])
-        print(data)
     except json.JSONDecodeError:
         reply += "`Error: Can't extract the link`\n"
         return reply
@@ -84,22 +87,26 @@ def mega_download(url: str) -> str:
     file_url = data['url']
     file_hex = data['hex']
     file_raw_hex = data['raw_hex']
-    if wget.download(file_url, out=file_name):
-        encrypt_file(file_name, file_hex, file_raw_hex)
-        reply += (f"`{file_name}`\n"
-                  f"Size: {file_size}\n"
-                  "\n"
-                  "Successfully downloaded...")
-    else:
-        reply += "Failed to download..."
+    if exists(file_name):
+        os.remove(file_name)
+    if not exists(file_name):
+        wget.download(file_url, out=file_name)
+        if exists(file_name):
+            encrypt_file(file_name, file_hex, file_raw_hex)
+            reply += (f"`{file_name}`\n"
+                      f"Size: {file_size}\n\n"
+                      "Successfully downloaded...")
+        else:
+            reply += "Failed to download..."
     return reply
 
 
 def encrypt_file(file_name, file_hex, file_raw_hex):
-    os.rename(file_name, f"old_{file_name}")
-    cmd = f"cat old_{file_name} | openssl enc -d -aes-128-ctr -K {file_hex} -iv {file_raw_hex} > {file_name}"
+    os.rename(file_name, r"old_{}".format(file_name))
+    cmd = ("cat 'old_{}' | openssl enc -d -aes-128-ctr -K {} -iv {} > '{}'"
+           .format(file_name, file_hex, file_raw_hex, file_name))
     subprocess_run(cmd)
-    os.remove(f"old_{file_name}")
+    os.remove(r"old_{}".format(file_name))
     return
 
 
