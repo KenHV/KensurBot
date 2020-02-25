3d4ed1fecc40514dfb7dd2b1f7f67da3893bfaef
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-02-25 18:03:22 +0530

    Update lydia.py

--- userbot/modules/lydia.py
+++ userbot/modules/lydia.py
@@ -11,6 +11,9 @@ Userbot module to use an AI To respond to people
 """
 
 import coffeehouse
+from coffeehouse.lydia import LydiaAI
+from coffeehouse.api import API
+
 import asyncio
 from userbot import LYDIA_API_KEY
 
