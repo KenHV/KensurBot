ab22ede97976c1a553a7bfff77d4f24127107237
Adek Maulana <adek@techdro.id>
2020-03-06 20:57:46 +0700

    mega_dowloader: fixed locking bot when in Combining status
    Change-Id: If4242b11a88728acb631004bdb97d5db9f010cfa

--- userbot/modules/mega_downloader.py
+++ userbot/modules/mega_downloader.py
@@ -21,6 +21,7 @@
 from asyncio import create_subprocess_shell as asyncSubprocess
 from asyncio.subprocess import PIPE as asyncPIPE
 
+import asyncio
 import re
 import json
 import os
@@ -109,14 +110,14 @@ async def mega_downloader(megadl):
                 f" @ {speed}"
                 f"\nETA: {estimated_total_time}"
             )
-            if status == "Downloading" and display_message != current_message:
+            if display_message != current_message:
                 await megadl.edit(current_message)
-                display_message = current_message
-            elif status == "Combining" and current_message != display_message:
-                await megadl.edit(current_message)
+                await asyncio.sleep(0.2)
                 display_message = current_message
         except Exception:
             pass
+        if status == "Combining":
+            await asyncio.sleep(estimated_total_time)
     if downloader.isSuccessful():
         download_time = downloader.get_dl_time(human=True)
         try:
