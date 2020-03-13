69908c6025846ed9ff72beb7781e44823888d1ff
Adek Maulana <adek@techdro.id>
2020-03-13 23:00:48 +0700

    scrapers: .img: set image to 7
    Change-Id: I8ec16852d307fdbde83abdb4ca637f5e3ccc1b80

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -123,7 +123,7 @@ async def carbon_api(e):
 @register(outgoing=True, pattern="^.img (.*)")
 async def img_sampler(event):
     """ For .img command, search and return images matching the query. """
-    await event.edit("Processing...")
+    await event.edit("`Processing...`")
     query = event.pattern_match.group(1)
     lim = findall(r"lim=\d+", query)
     try:
@@ -131,7 +131,7 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 3
+        lim = 7
     response = googleimagesdownload()
 
     # creating list of arguments
