f28501ad7b789f8832d6e6a4da6005a19880944e
Adek Maulana <adek@techdro.id>
2020-03-22 19:20:54 +0700

    heroku: use proper documentation for dyno hours
    Change-Id: Ibf2862627efce5f3c30c42ef0ec40d5fe98aaec3

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -5,6 +5,10 @@
 """
 
 import heroku3
+import asyncio
+
+from asyncio import create_subprocess_shell as asyncSubprocess
+from asyncio.subprocess import PIPE as asyncPIPE
 
 from userbot import CMD_HELP, LOGS, HEROKU_APP_NAME, HEROKU_API_KEY
 from userbot.events import register
@@ -12,17 +16,35 @@ from userbot.events import register
 Heroku = heroku3.from_key(HEROKU_API_KEY)
 
 
+async def subprocess_run(cmd, heroku):
+    subproc = await asyncSubprocess(cmd, stdout=asyncPIPE, stderr=asyncPIPE)
+    stdout, stderr = await subproc.communicate()
+    exitCode = subproc.returncode
+    if exitCode != 0:
+        await heroku.edit(
+            '**An error was detected while running subprocess**\n'
+            f'```exitCode: {exitCode}\n'
+            f'stdout: {stdout.decode().strip()}\n'
+            f'stderr: {stderr.decode().strip()}```')
+        return exitCode
+    return stdout.decode().strip(), stderr.decode().strip(), exitCode
+
+
 @register(outgoing=True, pattern=r"^.heroku(?: |$)(.*)")
 async def heroku_manager(heroku):
     await heroku.edit("`Processing...`")
+    await asyncio.sleep(3)
     conf = heroku.pattern_match.group(1)
-    hours_remaining = Heroku.ratelimit_remaining()
-    await heroku.edit(f"**Heroku** rate limit remaining: {hours_remaining}")
+    result = await subprocess_run(f'heroku ps -a {HEROKU_APP_NAME}', heroku)
+    if result[2] != 0:
+        return
+    hours_remaining = result[0]
+    await heroku.edit('`' + hours_remaining + '`')
     return
 
 
 CMD_HELP.update({
     "heroku":
     "`.heroku`"
-    "Usage: Your heroku account manager"
+    "Usage: Check your heroku dyno hours remaining"
 })
