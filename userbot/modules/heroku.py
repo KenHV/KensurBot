c0cba6e04de479357758f109c9f6ccea2b26e16a
Adek Maulana <adek@techdro.id>
2020-03-26 22:25:50 +0700

    heroku: add feature to set/get config vars
    Change-Id: I8a017a9a66f1ebfbf8a8cf495a9273de6917f8a9

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -4,15 +4,16 @@
    Heroku manager for your userbot
 """
 
-import os
 import heroku3
 import asyncio
+import os
 
 from asyncio import create_subprocess_shell as asyncSubprocess
 from asyncio.subprocess import PIPE as asyncPIPE
 
 from userbot import CMD_HELP, LOGS, HEROKU_APP_NAME, HEROKU_API_KEY
 from userbot.events import register
+from userbot.prettyjson import prettyjson
 
 Heroku = heroku3.from_key(HEROKU_API_KEY)
 
@@ -36,41 +37,41 @@ async def variable(var):
     if HEROKU_APP_NAME is not None:
         app = Heroku.app(HEROKU_APP_NAME)
     else:
-        var.edit("`[HEROKU]:\nPlease setup your` **HEROKU_APP_NAME**")
+        await var.edit("`[HEROKU]:\nPlease setup your` **HEROKU_APP_NAME**")
+        return
     exe = var.pattern_match.group(1)
     heroku_var = app.config()
     if exe == "get":
         await var.edit("`Getting information...`")
-        with open('configs.txt', 'w+') as variables:
-            for config, value in heroku_var:
-                variables.write(f"{config}: {value}" + "\n")
-        result = open('configs.txt').read()
-        if len(result) >= 4096:
-            await var.client.send_file(
-                var.chat_id,
-                "configs.txt",
-                reply_to=var.id,
-                caption="`Output configs is too large, sending it as file...`",
-            )
-        else:
-            var.edit("`[HEROKU]`:\n"
-                     "================================"
-                     f"    **{HEROKU_APP_NAME}**  configs are:"
-                     "================================"
-                     f"\n\n```{result}```"
-                     )
-        result.close()
-        os.remove('configs.txt')
+        configs = prettyjson(heroku_var.to_dict(), indent=2)
+        with open("configs.json", "w") as fp:
+            fp.write(configs)
+        with open("configs.json", "r") as fp:
+            result = fp.read()
+            if len(result) >= 4096:
+                await var.client.send_file(
+                    var.chat_id,
+                    "configs.json",
+                    reply_to=var.id,
+                    caption="`Output too large, sending it as a file`",
+                )
+            else:
+                await var.edit("`[HEROKU]` variables:\n\n"
+                               "================================"
+                               f"\n```{result}```\n"
+                               "================================"
+                               )
+        os.remove("configs.json")
         return
     else:
         await var.edit("`Setting information...`")
         val = var.pattern_match.group(2).split()
-        heroku_var[val[0]] = val[1]
         await asyncio.sleep(3)
         if val[0] in heroku_var:
             await var.edit(f"**{val[0]}**  `successfully changed to`  **{val[1]}**")
         else:
             await var.edit(f"**{val[0]}**  `successfully added with value: **{val[1]}**")
+        heroku_var[val[0]] = val[1]
         return
 
 
