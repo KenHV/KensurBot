22d4f3939b166c875e8e4d922f8e658dbff156ea
Adek Maulana <adek@techdro.id>
2020-03-30 21:42:14 +0700

    google_drive: manage folder (make|delete)
    Change-Id: I6abbc76b84ea5feaf0d4c0e55bf08aba4fb2690f

--- userbot/modules/google_drive.py
+++ userbot/modules/google_drive.py
@@ -7,16 +7,18 @@
 """
 import pickle
 import codecs
-from os.path import isfile
+from os.path import isfile, isdir
+from mimetypes import guess_type
 
 from telethon import events
 
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from google.auth.transport.requests import Request
 
 from userbot import (G_DRIVE_CLIENT_ID, G_DRIVE_CLIENT_SECRET,
-                     G_DRIVE_AUTH_TOKEN_DATA, GDRIVE_FOLDER_ID, BOTLOG_CHATID,
+                     G_DRIVE_AUTH_TOKEN_DATA, BOTLOG_CHATID,
                      TEMP_DOWNLOAD_DIRECTORY, CMD_HELP, LOGS)
 from userbot.events import register
 
@@ -32,13 +34,26 @@ REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 # ============================================================ #
 
 
-@register(pattern="^.gdf (.*)", outgoing=True)
-async def test(gdrive):
-    service = await services(gdrive)
-    folder_name = gdrive.pattern_match.group(1)
+async def get_mimeType(name):
+    """ - Check mimeType given file - """
+    if isfile(name):
+        mimeType = guess_type(name)[0]
+        if not mimeType:
+            mimeType = 'text/plain'
+    return mimeType
+
+
+@register(pattern=r"^.gdf (mkdir|rm)(.*)", outgoing=True)
+async def folders(gdrive):
+    """ - Google Drive folder management - """
+    URL = "https://drive.google.com/drive/folders/"
+    await gdrive.edit("`Sending information...`")
+    service = await create_app(gdrive)
+    folder_name = gdrive.pattern_match.group(2).strip()
+    exe = gdrive.pattern_match.group(1)
     folder_metadata = {
         'name': folder_name,
-        'mimeType': 'application/vnd.google-apps.folder'
+        'mimeType': 'application/vnd.google-apps.folder',
     }
     page_token = None
     result = service.files().list(
@@ -50,22 +65,54 @@ async def test(gdrive):
         ),
         pageToken=page_token
      ).execute()
-    try:
-        folder = result.get('files', [])[0]
-    except IndexError:
-        return await gdrive.edit(f"**{folder_name}**  `is not exists`")
-    else:
-        f_name = folder.get('name')
-        f_id = folder.get('id')
+    if exe == "mkdir":
+        """ - Create a directory, abort if exist when parent not given - """
+        try:
+            folder = result.get('files', [])[0]
+        except IndexError:
+            folder = service.files().create(
+                   body=folder_metadata,
+                   fields='id'
+             ).execute()
+            folder_id = folder.get('id')
+            await gdrive.edit(
+                "`[FOLDER - CREATED]`\n\n"
+                f" • `Name :` `{folder_name}`\n"
+                f" • `ID   :` `{folder_id}`\n"
+                f" • `URL  :` {URL + folder_id}"
+            )
+        else:
+            await gdrive.edit(
+                "`[FOLDER - EXIST]`\n\n"
+                f" • `Name :` `{folder_name}`")
+    elif exe == "rm":
+        """ - Permanently delete, skipping the trash - """
+        try:
+            """ - Try if given value is a name not a folderID - """
+            folder = result.get('files', [])[0]
+            folder_id = folder.get('id')
+        except IndexError:
+            """ - If failed assumming value is folderID - """
+            folder_id = folder_name
+        page_token = result.get('nextPageToken', None)
+        try:
+            service.files().delete(fileId=folder_id).execute()
+        except HttpError as e:
+            await gdrive.edit(
+                "`[FOLDER - ERROR]`\n\n"
+                f" • `Status :` `{str(e)}`"
+            )
+        else:
+            await gdrive.edit(
+                    "`[FOLDER - DELETE]`\n\n"
+                    " • `Status :` `OK`"
+            )
     page_token = result.get('nextPageToken', None)
-    return await gdrive.edit(f"**{f_name}** is exists")
+    return
 
 
-async def services(gdrive):
-    """
-        Generate credentials
-    """
-    await gdrive.edit("`Setting information...`")
+async def generate_credentials(gdrive):
+    """ - Generate credentials - """
     configs = {
         "installed": {
             "client_id": G_DRIVE_CLIENT_ID,
@@ -76,7 +123,7 @@ async def services(gdrive):
     }
     creds = None
     if G_DRIVE_AUTH_TOKEN_DATA is not None:
-        # decoded creds string to class object
+        """ - Repack credential objects from strings - """
         creds = pickle.loads(
               codecs.decode(G_DRIVE_AUTH_TOKEN_DATA.encode(), "base64"))
     else:
@@ -103,11 +150,18 @@ async def services(gdrive):
                 code = r.message.message.strip()
                 flow.fetch_token(code=code)
                 creds = flow.credentials
+            """ - Unpack credential objects into strings - """
             if G_DRIVE_AUTH_TOKEN_DATA is None:
                 with open("auth.txt", "w") as f:
-                    # save credentials object as a string
                     pickled = codecs.encode(
                             pickle.dumps(creds), "base64").decode()
+                    """ - Put into file to use it later - """
                     f.write(pickled)
+    return creds
+
+
+async def create_app(gdrive):
+    """ - Create google drive service app - """
+    creds = await generate_credentials(gdrive)
     service = build('drive', 'v3', credentials=creds, cache_discovery=False)
     return service
