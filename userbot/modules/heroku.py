630148afb8121bb65b788d17eff72492cea854f0
Adek Maulana <adek@techdro.id>
2020-03-30 13:53:07 +0700

    heroku: update regex for .set variable value
    get everything from value including newline, because value can be anything
    
    Change-Id: Ib9639dc00cc7375ea9084c5a80e3c54141efa0c4

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -18,7 +18,7 @@ Heroku = heroku3.from_key(HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
 
 
-@register(outgoing=True, pattern=r"^.(set|get|del) var(?: |$)(.*)(?: |$)")
+@register(outgoing=True, pattern=r"^.(set|get|del) var(?: |$)(.*)(?: |$)([\s\S]*)")
 async def variable(var):
     """
         Manage most of ConfigVars setting, set new var, get current var,
@@ -35,13 +35,13 @@ async def variable(var):
         await var.edit("`Getting information...`")
         await asyncio.sleep(1.5)
         try:
-            val = var.pattern_match.group(2).split()[0]
-            if val in heroku_var:
-                return await var.edit("**Config vars**:"
-                                      f"\n\n`{val} = {heroku_var[val]}`\n")
+            variable = var.pattern_match.group(2).split()[0]
+            if variable in heroku_var:
+                return await var.edit("**ConfigVars**:"
+                                      f"\n\n`{variable} = {heroku_var[variable]}`\n")
             else:
-                return await var.edit("**Config vars**:"
-                                      f"\n\n`Error -> {val} not exists`")
+                return await var.edit("**ConfigVars**:"
+                                      f"\n\n`Error:\n-> {variable} don't exists`")
         except IndexError:
             configs = prettyjson(heroku_var.to_dict(), indent=2)
             with open("configs.json", "w") as fp:
@@ -56,7 +56,7 @@ async def variable(var):
                         caption="`Output too large, sending it as a file`",
                     )
                 else:
-                    await var.edit("`[HEROKU]` variables:\n\n"
+                    await var.edit("`[HEROKU]` ConfigVars:\n\n"
                                    "================================"
                                    f"\n```{result}```\n"
                                    "================================"
@@ -65,29 +65,28 @@ async def variable(var):
             return
     elif exe == "set":
         await var.edit("`Setting information...`")
-        val = var.pattern_match.group(2).split()
-        try:
-            val[1]
-        except IndexError:
+        variable = var.pattern_match.group(2)
+        value = var.pattern_match.group(3)
+        if not value:
             return await var.edit("`.set var <config name> <value>`")
         await asyncio.sleep(1.5)
-        if val[0] in heroku_var:
-            await var.edit(f"**{val[0]}**  `successfully changed to`  **{val[1]}**")
+        if variable in heroku_var:
+            await var.edit(f"**{variable}**  `successfully changed to`  ->  **{value}**")
         else:
-            await var.edit(f"**{val[0]}**  `successfully added with value: **{val[1]}**")
-        heroku_var[val[0]] = val[1]
+            await var.edit(f"**{variable}**  `successfully added with value`  ->  **{value}**")
+        heroku_var[variable] = value
     elif exe == "del":
-        await var.edit("`Getting information to deleting vars...`")
+        await var.edit("`Getting information to deleting variable...`")
         try:
-            val = var.pattern_match.group(2).split()[0]
+            variable = var.pattern_match.group(2).split()[0]
         except IndexError:
-            return await var.edit("`Please specify config vars you want to delete`")
+            return await var.edit("`Please specify ConfigVars you want to delete`")
         await asyncio.sleep(1.5)
-        if val in heroku_var:
-            await var.edit(f"**{val}**  `successfully deleted`")
-            del heroku_var[val]
+        if variable in heroku_var:
+            await var.edit(f"**{variable}**  `successfully deleted`")
+            del heroku_var[variable]
         else:
-            return await var.edit(f"**{val}**  `is not exists`")
+            return await var.edit(f"**{variable}**  `is not exists`")
 
 
 @register(outgoing=True, pattern=r"^.usage(?: |$)")
@@ -141,7 +140,7 @@ async def dyno_usage(dyno):
                            f" -> `Dyno usage for`  **{HEROKU_APP_NAME}**:\n"
                            f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
                            f"**|**  [`{AppPercentage}`**%**]"
-                           "\n"
+                           "\n\n"
                            " -> `Dyno hours quota remaining this month`:\n"
                            f"     •  `{hours}`**h**  `{minutes}`**m**  "
                            f"**|**  [`{percentage}`**%**]"
