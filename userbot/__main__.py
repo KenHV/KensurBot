1aa6ad674c92d7568cacded6d4ba8f0432970859
Adek Maulana <adek@techdro.id>
2020-04-02 00:57:29 +0700

    ProjectBish: bump version
    Change-Id: Ic1d656cffead9e54bba08fc9526509d9160b27ee

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,7 +26,7 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running ProjectBish UserBot version: [1.beta]")
+LOGS.info("You are running ProjectBish UserBot version: [1.beta.1]")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
