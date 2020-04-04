a926730099ad9bbf9acc99d9a011956c732ece8d
Adek Maulana <adek@techdro.id>
2020-04-04 16:27:41 +0700

    misc: .readme: update for new google drive setup
    Change-Id: I5eca3adae11d5352237c17f29a4dd6fcd3fd622a

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -75,7 +75,7 @@ async def reedme(e):
         "Here's something for you to read:\n"
         "\n[ProjectBishUserBot's README.md file](https://github.com/adekmaulana/ProjectBish/blob/master/README.md)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
-        "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-GDrive-11-02)"
+        "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
         "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
         "\n[Video Tutorial - 576p](https://mega.nz/#!ErwCESbJ!1ZvYAKdTEfb6y1FnqqiLhHH9vZg4UB2QZNYL9fbQ9vs)"
         "\n[Video Tutorial - 1080p](https://mega.nz/#!x3JVhYwR!u7Uj0nvD8_CyyARrdKrFqlZEBFTnSVEiqts36HBMr-o)"
@@ -101,7 +101,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "Repo [here](https://github.com/adekmaulana/ProjectBish) GitHub page."
+        "[Repo](https://github.com/adekmaulana/ProjectBish) GitHub's page."
     )
 
 
