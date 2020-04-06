2c9686786c43dca22df27bfece1ce414c6f3840a
goodmeow <harunbam3@gmail.com>
2020-04-07 01:27:49 +0700

    userbot / events : Critical Security Bug Fix (Assigned as DS-SA-35CA)
    There's a vulnerability on evaluation module which can cause a
    Inline Bot to have a remote privilege elevation to any account
    that running Paperplane prior to this commit.
    
    Issues at: https://github.com/RaphielGang/Telegram-Paperplane/issues/222
    
    Change-Id: Ia0a6c2f5122a9df65919dd25626df3afcb47dd13

--- userbot/events.py
+++ userbot/events.py
@@ -69,6 +69,7 @@ def register(**args):
                 return
 
             if check.via_bot_id and not insecure:
+                await check.respond("`Inline bots are disabled for security reasons`")
                 return
 
             try:
