9fa0d7eb4ee35aa84869f499429abd480d33cfaa
Adek Maulana <adek@techdro.id>
2020-04-20 15:17:02 +0700

    modules: deepfryer image Improvement
    Change-Id: I33eef26095f405ecf20bdac062cea1b697c95806
    Signed-off-by: goodmeow <harunbam3@gmail.com>
    
    Conflicts:
    	userbot/modules/deepfryer.py

--- userbot/modules/deepfryer.py
+++ userbot/modules/deepfryer.py
@@ -1,33 +1,8 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
 # Original source for the deepfrying code (used under the following license): https://github.com/Ovyerus/deeppyer
 
-# MIT License
-#
-# Copyright (c) 2017 Ovyerus
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-""" Userbot module for frying stuff. ported by @NeoMatrix90 """
+import os
+import asyncio
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 import io
 from random import randint, uniform
@@ -37,93 +12,66 @@ from userbot.events import register
 from PIL import Image, ImageEnhance, ImageOps
 from telethon.tl.types import DocumentAttributeFilename
 
-from telethon import events
-
-
-@register(pattern="^.deepfry(?: |$)(.*)", outgoing=True) 
-async def deepfryer(event):
-    try:
-        frycount = int(event.pattern_match.group(1))
-        if frycount < 1:
-            raise ValueError
-    except ValueError:
-        frycount = 1
-
-    if event.is_reply:
-        reply_message = await event.get_reply_message()
-        data = await check_media(reply_message)
-
-        if isinstance(data, bool):
-            await event.edit("`I can't deep fry that!`")
-            return
-    else:
-        await event.edit("`Reply to an image or sticker to deep fry it!`")
+@register(outgoing=True, pattern=r'^.df(:? |$)(\d)?')
+async def _(fry):
+    await fry.edit("`Sending information...`")
+    level = fry.pattern_match.group(2)
+    if fry.fwd_from:
         return
-
-    # download last photo (highres) as byte array
-    await event.edit("`Downloading media…`")
-    image = io.BytesIO()
-    await event.client.download_media(data, image)
-    image = Image.open(image)
-
-    # fry the image
-    await event.edit("`Deep frying media…`")
-    for _ in range(frycount):
-        image = await deepfry(image)
-
-    fried_io = io.BytesIO()
-    fried_io.name = "image.jpeg"
-    image.save(fried_io, "JPEG")
-    fried_io.seek(0)
-
-    await event.reply(file=fried_io)
-
-
-async def deepfry(img: Image) -> Image:
-    colours = (
-        (randint(50, 200), randint(40, 170), randint(40, 190)),
-        (randint(190, 255), randint(170, 240), randint(180, 250))
-    )
-
-    img = img.copy().convert("RGB")
-
-    # Crush image to hell and back
-    img = img.convert("RGB")
-    width, height = img.width, img.height
-    img = img.resize((int(width ** uniform(0.8, 0.9)), int(height ** uniform(0.8, 0.9))), resample=Image.LANCZOS)
-    img = img.resize((int(width ** uniform(0.85, 0.95)), int(height ** uniform(0.85, 0.95))), resample=Image.BILINEAR)
-    img = img.resize((int(width ** uniform(0.89, 0.98)), int(height ** uniform(0.89, 0.98))), resample=Image.BICUBIC)
-    img = img.resize((width, height), resample=Image.BICUBIC)
-    img = ImageOps.posterize(img, randint(3, 7))
-
-    # Generate colour overlay
-    overlay = img.split()[0]
-    overlay = ImageEnhance.Contrast(overlay).enhance(uniform(1.0, 2.0))
-    overlay = ImageEnhance.Brightness(overlay).enhance(uniform(1.0, 2.0))
-
-    overlay = ImageOps.colorize(overlay, colours[0], colours[1])
-
-    # Overlay red and yellow onto main image and sharpen the hell out of it
-    img = Image.blend(img, overlay, uniform(0.5, 0.9))
-    img = ImageEnhance.Sharpness(img).enhance(randint(5, 300))
-
-    return img
-
-
-async def check_media(reply_message):
-    if reply_message and reply_message.media:
-        if reply_message.photo:
-            data = reply_message.photo
-        elif reply_message.document:
-            if DocumentAttributeFilename(file_name='AnimatedSticker.tgs') in reply_message.media.document.attributes:
-                return False
-            if reply_message.gif or reply_message.video or reply_message.audio or reply_message.voice:
-                return False
-            data = reply_message.media.document
+    if not fry.reply_to_msg_id:
+        await fry.edit("`Reply to any user message photo...`")
+        return
+    reply_message = await fry.get_reply_message()
+    if not reply_message.media:
+        await fry.edit("`No image found to fry...`")
+        return
+    if reply_message.sender.bot:
+        await fry.edit("`Reply to actual user...`")
+        return
+    chat = "@image_deepfrybot"
+    message_id_to_reply = fry.message.reply_to_msg_id
+    async with fry.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(reply_message)
+            if level:
+                m = f"/deepfry {level}"
+                msg_level = await conv.send_message(
+                          m,
+                          reply_to=msg.id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            else:
+                response = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await fry.reply("`Please unblock` @image_deepfrybot`...`")
+            return
+        if response.text.startswith("Forward"):
+            await fry.edit("`Please disable your forward privacy setting...`")
         else:
-            return False
-    else:
-        return False
+            downloaded_file_name = await fry.client.download_media(
+                                 response.media,
+                                 TEMP_DOWNLOAD_DIRECTORY
+            )
+            await fry.client.send_file(
+                fry.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                reply_to=message_id_to_reply
+            )
+            """ - cleanup chat after completed - """
+            try:
+                msg_level
+            except NameError:
+                await fry.client.delete_messages(conv.chat_id,
+                                                 [msg.id, response.id])
+            else:
+                await fry.client.delete_messages(
+                    conv.chat_id,
+                    [msg.id, response.id, r.id, msg_level.id])
+    await fry.delete()
+    return os.remove(downloaded_file_name)
 
     if not data or data is None:
         return False
@@ -132,6 +80,7 @@ async def check_media(reply_message):
 
 CMD_HELP.update({
     "deepfry":
-    ".deepfry [number]\
-    \n Usage: Reply to an image or sticker to deepfry with value, more value more krispy."
+    ">`.df` or >`.df [level(1-8)]`"
+    "\nUsage: deepfry image/sticker from the reply."
+    "\n@image_deepfrybot"
 })
