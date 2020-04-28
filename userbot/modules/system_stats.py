7333ec17a7806a41306dd6aae77e26958dee9e3c
Hackintosh 5 <hackintosh5@wildfyre.net>
2020-04-28 08:36:01 +0100

    Fix CWE-78 in system_stats
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -5,7 +5,7 @@
 #
 """ Userbot module for getting information about the server. """
 
-from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from platform import python_version, uname
 from shutil import which
@@ -23,100 +23,109 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.sysd$")
 async def sysdetails(sysd):
     """ For .sysd command, get system info using neofetch. """
-    try:
-        neo = "neofetch --stdout"
-        fetch = await asyncrunapp(
-            neo,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
+    if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
+        try:
+            fetch = await asyncrunapp(
+                "neofetch",
+                "--stdout",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
 
-        stdout, stderr = await fetch.communicate()
-        result = str(stdout.decode().strip()) \
-            + str(stderr.decode().strip())
+            stdout, stderr = await fetch.communicate()
+            result = str(stdout.decode().strip()) \
+                + str(stderr.decode().strip())
 
-        await sysd.edit("`" + result + "`")
-    except FileNotFoundError:
-        await sysd.edit("`Install neofetch first !!`")
+            await sysd.edit("`" + result + "`")
+        except FileNotFoundError:
+            await sysd.edit("`Install neofetch first !!`")
 
 
 @register(outgoing=True, pattern="^.botver$")
 async def bot_ver(event):
     """ For .botver command, get the bot version. """
-    if which("git") is not None:
-        invokever = "git describe --all --long"
-        ver = await asyncrunapp(
-            invokever,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await ver.communicate()
-        verout = str(stdout.decode().strip()) \
-            + str(stderr.decode().strip())
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@",
+                                                             "!"):
+        if which("git") is not None:
+            ver = await asyncrunapp(
+                "git",
+                "describe",
+                "--all",
+                "--long",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+            stdout, stderr = await ver.communicate()
+            verout = str(stdout.decode().strip()) \
+                + str(stderr.decode().strip())
 
-        invokerev = "git rev-list --all --count"
-        rev = await asyncrunapp(
-            invokerev,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await rev.communicate()
-        revout = str(stdout.decode().strip()) \
-            + str(stderr.decode().strip())
+            rev = await asyncrunapp(
+                "git",
+                "rev-list",
+                "--all",
+                "--count",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+            stdout, stderr = await rev.communicate()
+            revout = str(stdout.decode().strip()) \
+                + str(stderr.decode().strip())
 
-        await event.edit("`Userbot Version: "
-                         f"{verout}"
-                         "` \n"
-                         "`Revision: "
-                         f"{revout}"
-                         "`")
-    else:
-        await event.edit(
-            "Shame that you don't have git, You're running 9.0 - 'Extended' anyway"
-        )
+            await event.edit("`Userbot Version: "
+                             f"{verout}"
+                             "` \n"
+                             "`Revision: "
+                             f"{revout}"
+                             "`")
+        else:
+            await event.edit(
+                "Shame that you don't have git, you're running - 'v1.beta.4' anyway!"
+            )
 
 
 @register(outgoing=True, pattern="^.pip(?: |$)(.*)")
 async def pipcheck(pip):
     """ For .pip command, do a pip search. """
-    pipmodule = pip.pattern_match.group(1)
-    if pipmodule:
-        await pip.edit("`Searching . . .`")
-        invokepip = f"pip3 search {pipmodule}"
-        pipc = await asyncrunapp(
-            invokepip,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
+    if not pip.text[0].isalpha() and pip.text[0] not in ("/", "#", "@", "!"):
+        pipmodule = pip.pattern_match.group(1)
+        if pipmodule:
+            await pip.edit("`Searching . . .`")
+            pipc = await asyncrunapp(
+                "pip3",
+                "search",
+                pipmodule,
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
 
-        stdout, stderr = await pipc.communicate()
-        pipout = str(stdout.decode().strip()) \
-            + str(stderr.decode().strip())
+            stdout, stderr = await pipc.communicate()
+            pipout = str(stdout.decode().strip()) \
+                + str(stderr.decode().strip())
 
-        if pipout:
-            if len(pipout) > 4096:
-                await pip.edit("`Output too large, sending as file`")
-                file = open("output.txt", "w+")
-                file.write(pipout)
-                file.close()
-                await pip.client.send_file(
-                    pip.chat_id,
-                    "output.txt",
-                    reply_to=pip.id,
-                )
-                remove("output.txt")
-                return
-            await pip.edit("**Query: **\n`"
-                           f"{invokepip}"
-                           "`\n**Result: **\n`"
-                           f"{pipout}"
-                           "`")
+            if pipout:
+                if len(pipout) > 4096:
+                    await pip.edit("`Output too large, sending as file`")
+                    file = open("output.txt", "w+")
+                    file.write(pipout)
+                    file.close()
+                    await pip.client.send_file(
+                        pip.chat_id,
+                        "output.txt",
+                        reply_to=pip.id,
+                    )
+                    remove("output.txt")
+                    return
+                await pip.edit("**Query: **\n`"
+                               f"pip3 search {pipmodule}"
+                               "`\n**Result: **\n`"
+                               f"{pipout}"
+                               "`")
+            else:
+                await pip.edit("**Query: **\n`"
+                               f"pip3 search {pipmodule}"
+                               "`\n**Result: **\n`No Result Returned/False`")
         else:
-            await pip.edit("**Query: **\n`"
-                           f"{invokepip}"
-                           "`\n**Result: **\n`No Result Returned/False`")
-    else:
-        await pip.edit("`Use .help pip to see an example`")
+            await pip.edit("`Use .help pip to see an example`")
 
 
 @register(outgoing=True, pattern="^.alive$")
