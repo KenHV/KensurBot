c9d8778d9f4f7c66be1be598e00b11673b3ff6d2
goodmeow <harunbam3@gmail.com>
2020-05-09 03:47:50 +0700

    userbot: import figlet module
    Signed-off-by: goodmeow <harunbam3@gmail.com>

---
+++ userbot/modules/figlet.py
@@ -0,0 +1,40 @@
+import asyncio
+import pyfiglet
+from telethon import events, functions
+from userbot import CMD_HELP
+from userbot.events import register
+import sys
+ 
+@register(outgoing=True, pattern="^.figlet(?: |$)(.*)")
+async def figlet(event):
+    if event.fwd_from:
+        return
+    CMD_FIG = {"slant": "slant", "3D": "3-d", "5line": "5lineoblique", "alpha": "alphabet", "banner": "banner3-D", "doh": "doh", "iso": "isometric1", "letter": "letters", "allig": "alligator", "dotm": "dotmatrix", "bubble": "bubble", "bulb": "bulbhead", "digi": "digital"}
+    input_str = event.pattern_match.group(1)
+    if "|" in input_str:
+        text, cmd = input_str.split("|", maxsplit=1)
+    elif input_str is not None:
+        cmd = None
+        text = input_str
+    else:
+        await event.edit("Please add some text to figlet")
+        return
+    if cmd is not None:
+        try:
+            font = CMD_FIG[cmd]
+        except KeyError:
+            await event.edit("Invalid selected font.")
+            return
+        result = pyfiglet.figlet_format(text, font=font)
+    else:
+        result = pyfiglet.figlet_format(text)
+    await event.respond("‌‌‎`{}`".format(result))
+    await event.delete()
+
+CMD_HELP.update({
+        "figlet": 
+        ".figlet "
+         " \nUsage: Enhance ur text to strip line with anvil."
+         " \n\nslant, 3D, 5line, alpha, banner, doh, iso, letter, allig,"
+         " \ndotm, bubble, bulb, digi. are available figures"
+    })
