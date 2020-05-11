f3286507357ae2345689b4113304084fed7303e9
Adek Maulana <adek@techdro.id>
2020-05-11 22:25:45 +0700

    heroku: use aiohttp instead of requests for usage
    Change-Id: I55ec37742fceb562322ab87607e97cc1737f06df
    
    Conflicts:
    	userbot/modules/heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -10,8 +10,7 @@
 """
 
 import heroku3
-import asyncio
-import requests
+import aiohttp
 import math
 
 from userbot import (
@@ -166,43 +165,53 @@ async def dyno_usage(dyno):
      'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
     }
     path = "/accounts/" + user_id + "/actions/get-quota"
-    r = requests.get(heroku_api + path, headers=headers)
-    if r.status_code != 200:
-        return await dyno.edit("`Error: something bad happened`\n\n"
-                               f">.`{r.reason}`\n")
-    result = r.json()
-    quota = result['account_quota']
-    quota_used = result['quota_used']
+    async with aiohttp.ClientSession() as session:
+        async with session.get(heroku_api + path, headers=headers) as r:
+            if r.status != 200:
+                await dyno.client.send_message(
+                    dyno.chat_id,
+                    f"`{r.reason}`",
+                    reply_to=dyno.id
+                )
+                await dyno.edit("`Can't get information...`")
+                return False
+            result = await r.json()
+            quota = result['account_quota']
+            quota_used = result['quota_used']
 
-    """ - Used - """
-    remaining_quota = quota - quota_used
-    percentage = math.floor(remaining_quota / quota * 100)
-    minutes_remaining = remaining_quota / 60
-    hours = math.floor(minutes_remaining / 60)
-    minutes = math.floor(minutes_remaining % 60)
+            """ - User Quota Limit and Used - """
+            remaining_quota = quota - quota_used
+            percentage = math.floor(remaining_quota / quota * 100)
+            minutes_remaining = remaining_quota / 60
+            hours = math.floor(minutes_remaining / 60)
+            minutes = math.floor(minutes_remaining % 60)
 
-    """ - Current - """
-    App = result['apps']
-    try:
-        App[0]['quota_used']
-    except IndexError:
-        AppQuotaUsed = 0
-        AppPercentage = 0
-    else:
-        AppQuotaUsed = App[0]['quota_used'] / 60
-        AppPercentage = math.floor(App[0]['quota_used'] * 100 / quota)
-    AppHours = math.floor(AppQuotaUsed / 60)
-    AppMinutes = math.floor(AppQuotaUsed % 60)
+            """ - User App Used Quota - """
+            Apps = result['apps']
+            for apps in Apps:
+                if apps.get('app_uuid') == app.id:
+                    AppQuotaUsed = apps.get('quota_used') / 60
+                    AppPercentage = math.floor(
+                        apps.get('quota_used') * 100 / quota)
+                    break
+            else:
+                AppQuotaUsed = 0
+                AppPercentage = 0
 
-    return await dyno.edit("**Dyno Usage**:\n\n"
-                           f" -> `Dyno usage for`  **{HEROKU_APP_NAME}**:\n"
-                           f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
-                           f"**|**  [`{AppPercentage}`**%**]"
-                           "\n\n"
-                           " -> `Dyno hours quota remaining this month`:\n"
-                           f"     •  `{hours}`**h**  `{minutes}`**m**  "
-                           f"**|**  [`{percentage}`**%**]"
-                           )
+            AppHours = math.floor(AppQuotaUsed / 60)
+            AppMinutes = math.floor(AppQuotaUsed % 60)
+
+            await dyno.edit(
+                 "**Dyno Usage**:\n\n"
+                 f" -> `Dyno usage for`  **{app.name}**:\n"
+                 f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
+                 f"**|**  [`{AppPercentage}`**%**]"
+                 "\n\n"
+                 " -> `Dyno hours quota remaining this month`:\n"
+                 f"     •  `{hours}`**h**  `{minutes}`**m**  "
+                 f"**|**  [`{percentage}`**%**]"
+            )
+            return True
 
 
 CMD_HELP.update({
