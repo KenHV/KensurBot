7cb31a4ba5bdcadfb6d7c1f807ef8fd9c72238a3
Adek Maulana <adek@techdro.id>
2020-05-19 01:28:42 +0700

    google_drive: update progress every 15 seconds
    Change-Id: I87a04a18253476c522eb2a14c27acb47b4c63a30
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/google_drive.py
+++ userbot/modules/google_drive.py
@@ -452,7 +452,7 @@ async def download_gdrive(gdrive, service, uri):
                         f"`ETA` -> {time_formatter(eta)}"
                     )
                     if round(
-                      diff % 10.00) == 0 and (display_message
+                      diff % 15.00) == 0 and (display_message
                                               != current_message) or (
                       downloaded == file_size):
                         await gdrive.edit(current_message)
@@ -497,7 +497,9 @@ async def download_gdrive(gdrive, service, uri):
                         f" @ {humanbytes(speed)}`\n"
                         f"`ETA` -> {time_formatter(eta)}"
                     )
-                    if display_message != current_message or (
+                    if round(
+                      diff % 15.00) == 0 and (display_message
+                                              != current_message) or (
                       downloaded == file_size):
                         await gdrive.edit(current_message)
                         display_message = current_message
@@ -654,7 +656,8 @@ async def upload(gdrive, service, file_path, file_name, mimeType):
                 f"@ {humanbytes(speed)}`\n"
                 f"`ETA` -> {time_formatter(eta)}"
             )
-            if display_message != current_message or (
+            if round(diff % 15.00) == 0 and (
+              display_message != current_message) or (
               uploaded == file_size):
                 await gdrive.edit(current_message)
                 display_message = current_message
@@ -1265,7 +1268,7 @@ async def check_progress_for_dl(gdrive, gid, previous):
                     msg = previous
             else:
                 await gdrive.edit(f"`{msg}`")
-            await asyncio.sleep(5)
+            await asyncio.sleep(15)
             await check_progress_for_dl(gdrive, gid, previous)
             file = aria2.get_download(gid)
             complete = file.is_complete
