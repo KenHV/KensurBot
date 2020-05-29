e71c56e8d0acce381c51bc6c44f3b322ee8ae59d
aidilaryanto <aidilaryanto18@gmail.com>
2020-05-29 12:51:24 +0800

    getmusic: add DeezLoader module
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -38,6 +38,31 @@ async def _(event):
                                        [msg.id, respond.id])
     await event.delete()
 
+@register(outgoing=True, pattern="^.sdd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    d_link = event.pattern_match.group(1)
+    if ".com" not in d_link:
+        await event.edit("` I need a link to download something pro.`**(._.)**")
+    else:
+        await event.edit("**Initiating Download!**")
+    chat = "@DeezLoadBot"
+    async with bot.conversation(chat) as conv:
+          try:
+              msg = await conv.send_message(d_link)
+              response = await conv.get_response()
+              song = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+              await bot.send_file(event.chat_id, song)
+              await event.client.delete_messages(conv.chat_id,
+                                                 [msg.id, song.id,  response.id])
+              await event.delete()
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
+              return
+
 @register(outgoing=True, pattern="^.smd(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
@@ -67,7 +92,9 @@ async def _(event):
 CMD_HELP.update({
     "getmusic":
     ">`.netease <Artist - Song Title>`"
-    "\nUsage: Download music with @WooMaiBot\n"
-    ">`.smd <Artist - Song Title>`"
-    "\nUsage: Download music from Spotify\n"
+    "\nUsage: Download music with @WooMaiBot"
+    "\n\n>`.sdd <Spotify/Deezer Link>`"
+    "\nUsage: Download music from Spotify or Deezer"
+    "\n\n>`.smd <Artist - Song Title>`"
+    "\nUsage: Download music from Spotify"
 })
