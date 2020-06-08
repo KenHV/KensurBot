378298a8181b17d0fe0cf22bea151915f8448247
aidilaryanto <aidilaryanto18@gmail.com>
2020-06-09 00:58:38 +0800

    getmusic: change Deezload to MusicHunter
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -45,23 +45,22 @@ async def _(event):
         await event.edit("` I need a link to download something pro.`**(._.)**")
     else:
         await event.edit("**Initiating Download!**")
-    chat = "@DeezLoadBot"
+    chat = "@MusicHuntersBot"
     async with bot.conversation(chat) as conv:
           try:
               msg_start = await conv.send_message("/start")
               response = await conv.get_response()
-              r = await conv.get_response()
               msg = await conv.send_message(d_link)
               details = await conv.get_response()
               song = await conv.get_response()
               """ - don't spam notif - """
               await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError:
-              await event.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
+              await event.edit("**Error:** `unblock` @MusicHuntersBot `and retry!`")
               return
           await bot.send_file(event.chat_id, song, caption=details.text)
           await event.client.delete_messages(conv.chat_id,
-                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
+                                             [msg_start.id, response.id, msg.id, details.id, song.id])
           await event.delete()
 
 @register(outgoing=True, pattern="^.smd(?: |$)(.*)")
