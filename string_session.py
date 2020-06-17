843a1e212055000998199637388738a18f0b5441
Mr.Miss <keselekpermen69@gmail.com>
2020-06-17 10:47:10 +0700

    string_session: make easier to copy session string
    Now the STRING_SESSION value is send to your TG Saved Messages.
    
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>
    Signed-off-by: KenHV <kenhv@null.net>

--- string_session.py
+++ string_session.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python3
+# (c) https://t.me/TelethonChat/37677 and SpEcHiDe
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
 print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
-Create a new application, by entering the required details""")
-
-API_KEY = input("API_KEY: ")
-API_HASH = input("API_HASH: ")
+Create a new application, by entering the required details
+Check your Telegram saved messages section to copy the STRING_SESSION""")
+API_KEY = int(input("Enter API_KEY here: "))
+API_HASH = input("Enter API_HASH here: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print("Here is your userbot srting, copy it to a safe place !!")
-    print("")
-    print(client.session.save())
-    print("")
-    print("")
-    print("Enjoy your userbot!")
+    print("Check your Telegram Saved Messages to copy the STRING_SESSION value")
+    session_string = client.session.save()
+    saved_messages_template = """Support: @userbotindo
+
+<code>STRING_SESSION</code>: <code>{}</code>
+
+⚠️ <i>Please be carefull to pass this value to third parties</i>""".format(session_string)
+    client.send_message("me", saved_messages_template, parse_mode="html")
