9a68c41eebf913fb64e3dfff2e80cd604f472e85
KenHV <kenhv@null.net>
2020-06-29 12:57:36 +0530

    pmpermit: normie-fy pmpermit message
    some people were getting scared of the message
    
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -17,9 +17,8 @@ from userbot.events import register
 
 # ========================= CONSTANTS ============================
 UNAPPROVED_MSG = (
-    "`Yo, yo, yo! 1-4-8-3 to the 3 to the 6 to the 9, representin' the ABQ. What up, Biatch?\n\n`"
-    "`State the reason for your PM and wait till my Führer approves you.\n`"
-    "`Spamming will lead to you getting blocked and reported.`")
+    "`Hey there! Unfortunately, I don't accept private messages from strangers.\n`"
+    "`Please contact me in a group, or wait for me to approve you.`")  # thanks hackintosh5
 # =================================================================
 
 
