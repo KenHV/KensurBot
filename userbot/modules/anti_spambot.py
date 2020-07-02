a3bc2677c0d35fe92d4edd27ff13f42b92319d84
Mr.Miss <keselekpermen69@gmail.com>
2020-07-02 07:19:23 +0700

    anti_spambot: update CAS api
    https://t.me/combotnews/289
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>
    Signed-off-by: Angga <angga@linuxmail.org>
    
    Change-Id: I9d03873031a66c3de896007746433ac632076aec
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/anti_spambot.py
+++ userbot/modules/anti_spambot.py
@@ -82,13 +82,11 @@ async def ANTI_SPAMBOTS(welcm):
                     ##############################################
 
                     try:
-                        cas_url = f"https://combot.org/api/cas/check?user_id={check_user.id}"
+                        cas_url = f"https://api.cas.chat/check?user_id={check_user.id}" # https://t.me/combotnews/283
                         r = get(cas_url, timeout=3)
                         data = r.json()
                     except BaseException:
-                        print(
-                            "CAS check failed, falling back to legacy anti_spambot behaviour."
-                        )
+                        print("CAS check failed, falling back to legacy anti_spambot behaviour.")
                         data = None
                         pass
 
