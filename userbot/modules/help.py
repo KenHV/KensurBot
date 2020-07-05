638b791bc7c7cc678557212592bfacbccaddef75
Angga <angga@linuxmail.org>
2020-07-05 14:47:39 +0530

    help: rework module
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot help command """
@@ -13,20 +13,20 @@ from userbot.events import register
 async def help(event):
     """ For .help command,"""
     args = event.pattern_match.group(1).lower()
+    # Prevent Channel Bug to get any information and command from all modules
+    if event.is_channel and not event.is_group:
+        await event.edit("`Help command isn't permitted on channels`")
+        return
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
             await event.edit("Please specify a valid module name.")
     else:
-        unsorted = ""
-        sorted = ""
+        string = "**List of all loaded module(s)**\n\
+                 \nSpecify which module do you want help for! \
+                 \n**Usage:** `.help` <module name>\n\n•    "
         for i in CMD_HELP:
-            unsorted += str(i) + " "
-        unsorted = unsorted.split()
-        unsorted.sort()
-        for i in unsorted:
-            sorted += str(i) + " | "
-        help_message = ("**To view help for specific module, do `.help` <module_name>"
-                f"\nList of available modules:**\n\n{sorted[:-3]}")
-        await event.edit(help_message)
+            string += "`" + str(i)
+            string += "`\t\t\t•\t\t\t "
+        await event.edit(f"{string}")
