e2e9121e524240227d4b20dff2a7e09f42474836
Mr.Miss <keselekpermen69@gmail.com>
2020-07-14 21:22:17 +0700

    zipfile: add upload progress and improve
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/zipfile.py
+++ userbot/modules/zipfile.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# Port from UniBorg to Userbot by yincen17
+# Port from UniBorg by yincen17
 
 import asyncio
 import os
@@ -32,7 +32,7 @@ async def _(event):
     if not event.is_reply:
         await event.edit("`Reply to a file to compress it.`")
         return
-    mone = await event.edit("`Processing ...`")
+    mone = await event.edit("`Processing...`")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     if event.reply_to_msg_id:
@@ -43,23 +43,24 @@ async def _(event):
                 reply_message,
                 TEMP_DOWNLOAD_DIRECTORY,
                 progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(d, t, mone, c_time, "`Trying to download`")
-                )
-            )
+                    progress(d, t, mone, c_time, "[DOWNLOADING]")))
             directory_name = downloaded_file_name
-            await event.edit(downloaded_file_name)
+            await event.edit(f"Downloaded to `{directory_name}`"
+                             "`\ncompressing file...`")
         except Exception as e:  # pylint:disable=C0103,W0703
             await mone.edit(str(e))
     zipfile.ZipFile(directory_name + '.zip', 'w',
                     zipfile.ZIP_DEFLATED).write(directory_name)
+    c_time = time.time()
     await bot.send_file(
         event.chat_id,
         directory_name + ".zip",
         force_document=True,
         allow_cache=False,
         reply_to=event.message.id,
-    )
-    await event.edit("`DONE!!!`")
+        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            progress(d, t, mone, c_time, "[UPLOADING]")))
+    await event.edit("`Done!!`")
     await asyncio.sleep(7)
     await event.delete()
 
@@ -76,7 +77,7 @@ async def addzip(add):
     if not add.is_reply:
         await add.edit("`Reply to a file to compress it.`")
         return
-    mone = await add.edit("`Processing ...`")
+    mone = await add.edit("`Processing...`")
     if not os.path.isdir(ZIP_DOWNLOAD_DIRECTORY):
         os.makedirs(ZIP_DOWNLOAD_DIRECTORY)
     if add.reply_to_msg_id:
@@ -87,9 +88,7 @@ async def addzip(add):
                 reply_message,
                 ZIP_DOWNLOAD_DIRECTORY,
                 progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(d, t, mone, c_time, "`Trying to download`")
-                )
-            )
+                    progress(d, t, mone, c_time, "[DOWNLOADING]")))
             success = str(downloaded_file_name).replace("./zips/", "")
             await add.edit(f"`{success} Successfully added to list`")
         except Exception as e:  # pylint:disable=C0103,W0703
@@ -100,22 +99,24 @@ async def addzip(add):
 @register(outgoing=True, pattern=r"^\.upzip(?: |$)(.*)")
 async def upload_zip(up):
     if not os.path.isdir(ZIP_DOWNLOAD_DIRECTORY):
-        await up.edit(f"`Files not found`")
+        await up.edit("`Files not found`")
         return
-    await up.edit(f"`Uploading.......`")
+    mone = await up.edit("`Zipping File...`")
     input_str = up.pattern_match.group(1)
     curdate = today.strftime("%m%d%y")
     title = str(input_str) if input_str else "zipfile" + f"{curdate}"
     zipf = zipfile.ZipFile(title + '.zip', 'w', zipfile.ZIP_DEFLATED)
     zipdir(ZIP_DOWNLOAD_DIRECTORY, zipf)
     zipf.close()
+    c_time = time.time()
     await bot.send_file(
         up.chat_id,
         title + ".zip",
         force_document=True,
         allow_cache=False,
         reply_to=up.message.id,
-    )
+        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            progress(d, t, mone, c_time, "[UPLOADING]", input_str)))
     os.rmdir(ZIP_DOWNLOAD_DIRECTORY)
     await up.delete()
 
@@ -123,7 +124,7 @@ async def upload_zip(up):
 @register(outgoing=True, pattern=r"^\.rmzip(?: |$)(.*)")
 async def remove_dir(rm):
     if not os.path.isdir(ZIP_DOWNLOAD_DIRECTORY):
-        await rm.edit(f"`Directory not found`")
+        await rm.edit("`Directory not found`")
         return
     os.rmdir(ZIP_DOWNLOAD_DIRECTORY)
     await rm.edit("`Zip list removed`")
@@ -139,12 +140,12 @@ def zipdir(path, ziph):
 
 CMD_HELP.update({
     "zipfile":
-        ".compress **[optional: <reply to file>]**\
-            \nUsage: make files to zip.\
-            \n.addzip **<reply to file>**\
-            \nUsage: add files to zip list.\
-            \n.upzip **[optional: <zip title>]**\
-            \nUsage: upload zip list.\
-            \n.rmzip **[optional: <zip title>]**\
-            \nUsage: clear zip list."
+    "`.compress` [optional: <reply to file >]"
+    "\nUsage: make files to zip."
+    "\n\n`.addzip` <reply to file >"
+    "\nUsage: add files to zip list."
+    "\n\n`.upzip` [optional: <zip title>]"
+    "\nUsage: upload zip list."
+    "\n\n`.rmzip` [optional: <zip title>]"
+    "\nUsage: clear zip list."
 })
