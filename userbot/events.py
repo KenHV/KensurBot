4ac9cebe3327abb2789f7c0e8f01d6ca18fb13cd
KenHV <kenhv@null.net>
2020-07-19 18:02:44 +0530

    Revert "events: fix derp"
    This reverts commit 80a3359a6bb84b444b10921ebf1196673db21d80.

--- userbot/events.py
+++ userbot/events.py
@@ -139,7 +139,7 @@ def register(**args):
                     file.close()
 
                     if LOGSPAMMER:
-                        await check.respond(
+                        await check.client.respond(
                             "`Sorry, my userbot has crashed."
                             "\nThe error logs are stored in the userbot's log chat.`"
                         )
