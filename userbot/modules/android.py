af944aa2faa017433afaae591a2d8e4aa9bb6cfe
Mr.Miss <keselekpermen@mrmiss.wtf>
2020-10-18 20:34:46 +0700

    android: fix for magisk canary
    Co-authored-by: soulr344 <bloodthirstyoff@gmail.com>
    Signed-off-by: Mr.Miss <keselekpermen@mrmiss.wtf>
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -23,11 +23,18 @@ async def magisk(request):
     magisk_dict = {
         "Stable": "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/stable.json",
         "Beta": "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/beta.json",
-        "Canary": "https://raw.githubusercontent.com/topjohnwu/magisk_files/canary/debug.json",
+        "Canary": "https://raw.githubusercontent.com/topjohnwu/magisk_files/canary/canary.json",
     }
     releases = "Latest Magisk Releases:\n"
     for name, release_url in magisk_dict.items():
         data = get(release_url).json()
+        if str(name) == "Canary":
+            data["magisk"]["link"] = "https://github.com/topjohnwu/magisk_files/raw/canary/" + \
+                data["magisk"]["link"]
+            data["app"]["link"] = "https://github.com/topjohnwu/magisk_files/raw/canary/" + \
+                data["app"]["link"]
+            data["uninstaller"]["link"] = "https://github.com/topjohnwu/magisk_files/raw/canary/" + \
+                data["uninstaller"]["link"]
         releases += (
             f'{name}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | '
             f'[APK v{data["app"]["version"]}]({data["app"]["link"]}) | '
