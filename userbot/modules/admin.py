5e03ce9d9cbb29d2d775a1b5d3afca0f90edc214
KenHV <kenhv@null.net>
2020-11-28 12:52:13 +0530

    admin/mute: don't crash if an admin tries to mute another admin
    this actually is supported by the bot, it'll delete the muted admin's
    messages if the person who muted has message nuking rights. the
    exception is caught in the function that deletes said messages but the
    bot crashes when trying to edit another admin's permissions.
    
    let's catch that exception and continue normally.
    
    Change-Id: I7a8eb0c6ccf832b4c84bc27fecc3487c607a7ee4
    Signed-off-by: KenHV <kenhv@null.net>

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -326,22 +326,25 @@ async def spider(spdr):
             await spdr.client(
                 EditBannedRequest(spdr.chat_id, user.id, MUTE_RIGHTS))
 
-            # Announce that the function is done
-            if reason:
-                await spdr.edit(f"**Muted successfully!**\nReason: {reason}")
-            else:
-                await spdr.edit("**Muted successfully!**")
-
-            # Announce to logging group
-            if BOTLOG:
-                await spdr.client.send_message(
-                    BOTLOG_CHATID,
-                    "#MUTE\n"
-                    f"USER: [{user.first_name}](tg://user?id={user.id})\n"
-                    f"CHAT: {spdr.chat.title}(`{spdr.chat_id}`)",
-                )
         except UserIdInvalidError:
             return await spdr.edit("**Uh oh my mute logic broke!**")
+        except UserAdminInvalidError:
+            pass
+
+        # Announce that the function is done
+        if reason:
+            await spdr.edit(f"**Muted successfully!**\nReason: {reason}")
+        else:
+            await spdr.edit("**Muted successfully!**")
+
+        # Announce to logging group
+        if BOTLOG:
+            await spdr.client.send_message(
+                BOTLOG_CHATID,
+                "#MUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {spdr.chat.title}(`{spdr.chat_id}`)",
+            )
 
 
 @register(outgoing=True, pattern=r"^\.unmute(?: |$)(.*)")
