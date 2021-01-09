6023bc9524cf25b06598dd985bfe91b41d33af19
BianSepang <bianconneri46@gmail.com>
2021-01-10 00:47:56 +0800

    upload_download: add time
    * also fix derp with filename
    
    Signed-off-by: BianSepang <bianconneri46@gmail.com>
    Change-Id: I8df20c35ca24ddf643bdd0ea4cfcc2379bde7b79
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- userbot/modules/upload_download.py
+++ userbot/modules/upload_download.py
@@ -14,11 +14,12 @@ import math
 import os
 import subprocess
 import time
+from datetime import datetime
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from pySmartDL import SmartDL
-from telethon.tl.types import DocumentAttributeVideo
+from telethon.tl.types import DocumentAttributeFilename, DocumentAttributeVideo
 
 from userbot import CMD_HELP, LOGS, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
@@ -86,9 +87,13 @@ async def download(target_file):
         try:
             replied = await target_file.get_reply_message()
             file = replied.document
-            file_name = replied.document.attributes[-1].file_name
-            outdir = TEMP_DOWNLOAD_DIRECTORY + file_name
+            attribs = replied.media.document.attributes
+            for attr in attribs:
+                if isinstance(attr, DocumentAttributeFilename):
+                    filename = attr.file_name
+            outdir = TEMP_DOWNLOAD_DIRECTORY + filename
             c_time = time.time()
+            start_time = datetime.now()
             with open(outdir, "wb") as f:
                 result = await download_file(
                     client=target_file.client,
@@ -98,10 +103,13 @@ async def download(target_file):
                         progress(d, t, target_file, c_time, "[DOWNLOAD]", input_str)
                     ),
                 )
+            dl_time = (datetime.now() - start_time).seconds
         except Exception as e:  # pylint:disable=C0103,W0703
             await target_file.edit(str(e))
         else:
-            await target_file.edit(f"**Downloaded to `{result.name}` successfully!**")
+            await target_file.edit(
+                f"**Downloaded to** `{result.name}` **in** `{dl_time} seconds`**!**"
+            )
     else:
         await target_file.edit("**Reply to a message to download to my local server.**")
 
@@ -115,7 +123,7 @@ async def get_video_thumb(file, output=None, width=90):
         stdout=subprocess.PIPE,
         stderr=subprocess.DEVNULL,
     )
-    await asyncio.sleep(1.5)
+    await asyncio.sleep(2.5)
     return output
 
 
@@ -129,6 +137,7 @@ async def upload(u_event):
     if os.path.exists(input_str):
         file_name = input_str.split("/")[-1]
         c_time = time.time()
+        start_time = datetime.now()
         with open(input_str, "rb") as f:
             result = await upload_file(
                 client=u_event.client,
@@ -138,6 +147,7 @@ async def upload(u_event):
                     progress(d, t, u_event, c_time, "[FILE - UPLOAD]", input_str)
                 ),
             )
+        up_time = (datetime.now() - start_time).seconds
         if input_str.lower().endswith(("mp4", "mkv", "webm")):
             thumb = await get_video_thumb(input_str, output="thumb_image.jpg")
             metadata = extractMetadata(createParser(input_str))
@@ -169,7 +179,7 @@ async def upload(u_event):
                 ],
             )
             os.remove(thumb)
-            await u_event.edit("Uploaded successfully !!")
+            await u_event.edit(f"Uploaded successfully in `{up_time}` seconds.")
         else:
             await u_event.client.send_file(
                 u_event.chat_id,
@@ -179,9 +189,9 @@ async def upload(u_event):
                 allow_cache=False,
                 reply_to=u_event.message.id,
             )
-            await u_event.edit("Uploaded successfully !!")
+            await u_event.edit(f"Uploaded successfully in `{up_time}` seconds.")
     else:
-        await u_event.edit("404: File Not Found")
+        await u_event.edit("`404: File Not Found`")
 
 
 CMD_HELP.update(
