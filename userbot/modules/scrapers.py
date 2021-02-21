029556f0fb49a54e36e481479acc3091b40d15b5
4amparaboy <76533278+4amparaboy@users.noreply.github.com>
2021-02-21 16:49:15 +0700

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -80,9 +80,7 @@ async def carbon_api(e):
         },
     }
     driver.execute("send_command", params)
-    driver.find_element_by_xpath("//button[@id='export-menu']").click()
-    driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-    driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
+    driver.find_element_by_css_selector('[data-cy="quick-export-button"]').click()
     await e.edit("**Processing...\n75%**")
     # Waiting for downloading
     while not os.path.isfile(file_path):
