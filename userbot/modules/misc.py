KenHV/KensurBot

commit cd1ebcd4173c05e1784a71b2d132fb562167561a

misc: rewrite restart and fixup shutdown
restart command was actually causing a crash, which prompted heroku to
restart the process. this is why it took so long to come back up.

as for shutdown, there shouldn't be an await statement there.

Change-Id: I64caa120fa1d4a4f03597b9d46a52e52bfacfad0
Signed-off-by: KenHV <yo@kenharris.xyz>

userbot/modules/misc.py
@@ -8,7 +8,7 @@
 
 import io
 import sys
-from os import execl
+from os import environ, execle
 from random import randint
 from time import sleep
 
@@ -49,22 +49,22 @@ async def sleepybot(time):
 @register(outgoing=True, pattern=r"^\.shutdown$")
 async def killthebot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("**Shutting down...**")
+    await event.edit("**Shutting down.**")
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n" "Bot shut down")
-    await bot.disconnect()
+    bot.disconnect()
 
 
 @register(outgoing=True, pattern=r"^\.restart$")
 async def killdabot(event):
     await event.edit("**Restarting...**")
     if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "#RESTART \n" "Bot Restarted")
-    await bot.disconnect()
+        await event.client.send_message(
+            BOTLOG_CHATID, "#RESTART \n" "Restarting bot..."
+        )
     # Spin a new instance of bot
-    execl(sys.executable, sys.executable, *sys.argv)
-    # Shut the existing one down
-    sys.exit()
+    args = [sys.executable, "-m", "userbot"]
+    execle(sys.executable, *args, environ)
 
 
 @register(outgoing=True, pattern=r"^\.readme$")

commit a3d2a5f76215d5e5fd70a3bbd245cc8d3185bcfb

misc/shutdown: fix derp
disconnect() needs to be used with await after all, as per
https://docs.telethon.dev/en/latest/modules/client.html#telethon.client.telegrambaseclient.TelegramBaseClient.disconnect
i blame pyright.

Fixes: cd1ebcd4173c05e1784a71b2d132fb562167561a
Change-Id: I3d0413f8339ea607f2163279f1558304c923b523
Signed-off-by: KenHV <yo@kenharris.xyz>

userbot/modules/misc.py
@@ -52,7 +52,7 @@ async def killthebot(event):
     await event.edit("**Shutting down.**")
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n" "Bot shut down")
-    bot.disconnect()
+    await bot.disconnect()
 
 
 @register(outgoing=True, pattern=r"^\.restart$")