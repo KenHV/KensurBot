c49b410eecf2dc2d59aed03a4757e0dbc351921a
KenHV <yo@kenharris.xyz>
2021-03-26 09:22:06 +0530

    kensurbot: set correct path for chrome
    this shouldn't be configurable anyway.
    
    Change-Id: Ib12df1a20ade620796e349d742cf5770f9bc953a
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -100,8 +100,8 @@ ALIVE_NAME = str(os.environ.get("ALIVE_NAME")) or None
 REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY") or None
 
 # Chrome Driver and Headless Google Chrome Binaries
-CHROME_DRIVER = os.environ.get("CHROME_DRIVER") or None
-GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN") or None
+CHROME_DRIVER = "/usr/bin/chromedriver"
+GOOGLE_CHROME_BIN = "/usr/bin/google-chrome"
 
 # OpenWeatherMap API Key
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID") or None
