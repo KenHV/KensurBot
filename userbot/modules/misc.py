37f549140f87a6fab8abf8d5b12fccd62fcab909
KenHV <yo@kenharris.xyz>
2021-06-08 12:31:53 +0530

    send: Fix errors when passing chat IDs
    Change-Id: Icd090973c09407c19c288bab918582d4c6964f11
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -143,6 +143,11 @@ async def send(event):
 
     chat = event.pattern_match.group(1)
     try:
+        chat = int(chat)
+    except ValueError:
+        pass
+
+    try:
         chat = await event.client.get_entity(chat)
     except (TypeError, ValueError):
         return await event.edit("**Invalid link provided!**")
